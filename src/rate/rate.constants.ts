93de092e5fc32db6afabbcd87d7a3f3acfe57da0
Andrew Stepanov <bababum1995@gmail.com>
2026-02-13 13:41:44 +0700

    feat: update rates TTL to 12 hours
    - Change the time to live for rates from 1 hour to 12 hours in rate.constants.ts, enhancing the caching duration for rate data.

--- src/rate/rate.constants.ts
+++ src/rate/rate.constants.ts
@@ -1,2 +1,2 @@
-// Time to live for rates in milliseconds (1 hour)
-export const RATES_TTL_MS = 60 * 60 * 1000;
+// Time to live for rates in milliseconds (12 hours)
+export const RATES_TTL_MS = 12 * 60 * 60 * 1000;
