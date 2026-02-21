30bbcb84981fb428f8d64abb6312be6c749279b3
Andrew Stepanov <bababum1995@gmail.com>
2026-02-21 11:53:57 +0700

    refactor: remove ensureFreshRates call from health check
    - Eliminate the call to ensureFreshRates in the health method of AppService, simplifying the health check response.
    - Adjust the constructor by removing the RateService dependency, streamlining the service's design.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
-import { RateService } from './rate/rate.service';
-
 @Injectable()
 export class AppService {
-  constructor(private readonly rateService: RateService) {}
-
   health() {
-    void this.rateService.ensureFreshRates();
-
     return { status: 'ok' };
   }
 }
